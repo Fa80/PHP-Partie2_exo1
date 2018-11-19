8b8b97b9624535b090e7df6c4387cb7bb4f876f8
root <ngomfa@gmail.com>
2018-11-19 14:52:12 +0100

    exo_1

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>PHP Partie2_Exo1</title>
+</head>
+<body>
+    <?php
+
+          $age = 17;
+      if($age >=18){
+        echo "Vous êtes majeur";
+      }else{
+          echo "Vous êtes mineur";
+      }
+    ?>
+</body>
+</html>
